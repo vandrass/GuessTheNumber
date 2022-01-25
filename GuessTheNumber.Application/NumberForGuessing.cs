900cfdbf6a74d7b6691cd8f0b3d5b4b0071aeeff
Ivan Goncharov <vandrass@gmail.com>
2022-01-25 10:15:59 +0200

    Add project files.

---
+++ GuessTheNumber.Application/NumberForGuessing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GuessTheNumber.Application
+{
+    public class NumberForGuessing
+    {
+        public NumberForGuessing()
+        {
+            SetRandomNumber();
+        }
+
+        public int Number { get; private set; }
+
+        public void SetRandomNumber()
+        {
+            var random = new Random();
+            Number = random.Next(0, 100);
+        }
+    }
+}
---
+++ GuessTheNumber.UI/Program.cs
@@ -0,0 +1,19 @@
+﻿using GuessTheNumber.Application;
+using System;
+
+namespace GuessTheNumber.UI
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var numb = new NumberForGuessing();
+
+            Console.WriteLine(numb.Number);
+
+
+
+
+        }
+    }
+}
